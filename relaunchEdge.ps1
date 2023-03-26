2a954685b1e3422370d1015323ac00698431c502
Toshi H <thboo39@gmail.com>
2023-03-25 20:04:45 -0700

    SHut Edge gracefully

--- relaunchEdge.ps1
+++ relaunchEdge.ps1
@@ -1,6 +1,6 @@
 # change the path below
-$path2html = "C:/Users/toshi/Dropbox/GitHub/save2file"
-Get-Process -Name "*Edge*" | Stop-Process
+$path2html = "$home\Documents\github\service-desk-sign-in"
+Get-Process -Name "*Edge*" | ForEach-Object {$_.CloseMainWindow() | Out-Null} | Stop-Process
 Start-Process -FilePath msedge -ArgumentList file:///$($path2html)/index.html
 Start-Process -FilePath msedge -ArgumentList file:///$($path2html)/show_walkins.html
 
